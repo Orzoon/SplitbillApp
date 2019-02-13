97419207c5095efd5a9415b31de55f87c64e7f7e
Binay <binaydhawa@gmail.com>
2019-02-13 13:30:20 +1100

    Login validation (password prompt validation may have some problems)

--- src/Components/SignIn.js
+++ src/Components/SignIn.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {NavLink,FormGroup, Input, Modal,ModalBody, ModalHeader,Form, Label, Button, Card, CardTitle, CardImg, Col } from 'reactstrap';
+import {NavLink,FormGroup,FormFeedback, Input, Modal,ModalBody, ModalHeader,Form, Label, Button, Card, CardTitle, CardImg, Col } from 'reactstrap';
 import '../Styles/homepage.css';
 /**
  * TO DO LIST:
@@ -20,13 +20,104 @@ import '../Styles/homepage.css';
             super(props);
             this.state ={
                 isModalOpen:false,
-                isSignUpOpen:false
+                isSignUpOpen:false,
+                
+                SignIn:{
+                    firstname:'',
+                    lastname:'',
+                    username:'',
+                    email:'',
+                    gender:'',
+                    password:'',
+                    password2:''
+                },
+
+               
+                    login_username:'',
+                    login_password:'',
+                
+                
+                SignIn_touched:
+                {
+                    firstname:false,
+                    lastname:false,
+                    username:false,
+                    email:false,
+                    gender:false,
+                    password:false,
+                    password2:false
+                },
+
+                Login_touched:{
+                    username:false,
+                    password:false
+                }
+     
             }
 
             this.toggleModal = this.toggleModal.bind(this);
             this.toggleSignUp = this.toggleSignUp.bind(this);
+            this.handleInputChange = this.handleInputChange.bind(this);
+            this.handleBlur_Login= this.handleBlur_Login.bind(this);
+    }
+    
+    validate_signIn(firstname, lastname, telnum,username, email,password1,password2,gender) 
+    {
+
+        const errors = {
+            firstname: '',
+            lastname: '',
+            telnum: '',
+            email: ''
+        };
     }
 
+    validate_Login(username,password)
+    {
+        const errors={
+            username:'',
+            password:'',
+        };
+        
+
+        if (this.state.Login_touched.username && username.length<=3)
+            {
+                    errors.username='Username needs to be greater then three characters';
+                
+            }
+
+
+        if (this.state.Login_touched.password)
+            {
+                errors.password='Please enter password';
+                console.log("password state "+this.state.Login_touched.password);
+
+            }
+
+    
+
+        return errors;
+            
+    }
+
+    handleBlur_Login = (field) => (evt) =>{
+        this.setState({
+                Login_touched: { ...this.state.Login_touched, [field]:true},
+            });
+    }
+
+    handleInputChange(event){
+        const target = event.target;
+        const value = target.type === 'checkbox' 
+                            ? target.checked 
+                            : target.value;
+        const name = event.target.name;
+
+        this.setState({
+            [name]:value
+        });
+
+    }
 
     toggleModal()
     {
@@ -53,6 +144,7 @@ import '../Styles/homepage.css';
 
     render()
     {
+        const errors = this.validate_Login(this.state.login_username,this.state.login_password);
         return(
             < >
                <Card className="container" id="logo">
@@ -66,7 +158,7 @@ import '../Styles/homepage.css';
                             <Button type="submit" class="btn btn-info" onClick={this.toggleSignUp}>
                                 Sign Up
                             </Button>
-                        </div>
+                        </div>  
                         <div className="col-lg-4">
                             <Button type="submit" class="btn btn-info" onClick={this.toggleModal}>
                                 Login
@@ -82,16 +174,33 @@ import '../Styles/homepage.css';
                     <ModalBody>
                         <Form onSubmit = {this.handleLogin}>
                         <FormGroup>
-                            <Label htmlFor ="username">username</Label>
-                            <Input type ="text" id="username" name="username"
-                                innerRef ={(input)=>this.username = input}/>
+                            <Label htmlFor ="login_username">Username</Label>
+                            <Input type ="text" id="login_username" name="login_username"
+                                placeholder="Username"
+                                value={this.state.login_username}
+                                valid={errors.username === ''}
+                                invalid={errors.username !== ''}
+                                onBlur={this.handleBlur_Login('username')}
+                                onChange={this.handleInputChange}/>
+
+                            <FormFeedback>{errors.username}</FormFeedback>
                         </FormGroup>
 
                         <FormGroup>
-                            <Label htmlFor ="password">Password</Label>
-                            <Input type ="text" id="password" name="password"
-                                innerRef={(input)=>this.password = input}/>
+                            <Label htmlFor ="login_password">Password</Label>
+                            <Input type ="text" id="login_password" name="login_password"
+                                 placeholder="Password"
+                                 valid={errors.password === ''}
+                                 invalid={errors.username !== ''}
+                                 value={this.state.login_password}
+                                 onBlur={this.handleBlur_Login('password')}
+                                 onChange={this.handleInputChange}/>
+                            
+                            <FormFeedback>{errors.password}</FormFeedback>
                         </FormGroup>
+
+                       
+
                         </Form>
                         <FormGroup>
                         Don't Have an account yet.  
